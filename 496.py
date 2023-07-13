35f62724c4f9a6409bdaf310aedc856ed70d9996
wangjiannan <wangjiannan@sjtu.edu.cn>
2023-07-13 17:22:37 +0800

    1st version of 496

---
+++ 496.py
@@ -0,0 +1,28 @@
+# 496.下一个更大元素
+'''
+nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+'''
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        out = []
+        for n1 in nums1:
+            temp = nums2[nums2.index(n1)+1:]
+            flag = 0
+            for i in temp:
+                if i > n1:
+                    out.append(i)
+                    flag = 1
+                    break
+            if flag == 0:
+                out.append(-1)
+        return out
+            
+
