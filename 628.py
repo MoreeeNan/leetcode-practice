355c88e89cb0db7769399cb10529bca35d798279
wangjiannan <wangjiannan@sjtu.edu.cn>
2023-07-21 14:16:30 +0800

    通过测试

--- 628.py
+++ 628.py
@@ -2,45 +2,29 @@
 '''
 给你一个整型数组 nums ，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
 '''
-class Solution(object):
-    @ staticmethod
-    def find1stPositive(nums):
-        # nums已经按照绝对值从大到小排列好
-        for num in nums:
-            if num > 0:
-                return num
-            
-    @ staticmethod
-    def find1stNegative(nums):
-        # nums已经按照绝对值从大到小排列好
-        for num in nums:
-            if num < 0:
-                return num
-            
+class Solution(object):           
     def maximumProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        nums = sorted(nums, key=abs, reverse=True)
-
-        a,b,c = nums[0:3]
         if len(nums) == 3:
-            return a*b*c
+            return nums[0]*nums[1]*nums[2]
         else:
-            # 正正负，找到下一个绝对值最大的正数令为c
-            if a>0 and b>0 and c<0:
-                c = self.find1stPositive(nums[3:])
-            # 正负正，找到下一个绝对值最大的负数令为c
-            elif a>0 and b<0 and c>0:
-                c = self.find1stNegative(nums[3:])
-            # 负正正，找到下一个绝对值最大的负数令为c
-            elif a<0 and b>0 and c>0:
-                c = self.find1stNegative(nums[3:])
-            # 负负负，找到下一个绝对值最大的正数令为c
-            elif a<0 and b<0 and c<0:
-                c = self.find1stPositive(nums[3:])
-            return a*b*c
+            nums.sort(reverse=True)
+            if nums[-1] > 0:
+                # 全为正数
+                return nums[0]*nums[1]*nums[2]
+            elif nums[0] < 0:
+                # 全为负数
+                return nums[0]*nums[1]*nums[2]
+            else:
+                # 有正有负
+                # 要么是三个最大正数相乘，要么是最大正数和两个最小负数的乘积
+                a,b,c = nums[0:3]
+                e,f = nums[-2:]
+                return max(a*b*c,a*e*f)
+
 
 
     
